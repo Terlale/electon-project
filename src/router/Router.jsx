d5b0653133e78090db03264720c2a8c583834280
Terlale <trllramazanova@gmail.com>
2023-09-21 21:06:46 +0400

    -

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/home/Home";
 import Singin from "../pages/signin/Signin";
-import Favorite from "../pages/favorite/Favorite";
+
 import Basket from "../pages/basket/Basket";
+import Favorite from "../pages/favorite/Favorite";
 
 const Router = () => {
   return (
@@ -11,7 +12,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/signin" element={<Singin/>}/>
-        <Route path="/favorite" element={<Favorite/>}/>
+        <Route path="/favourites" element={<Favorite/>}/>
         <Route path="/basket" element={<Basket/>}/>
       </Routes>
     </BrowserRouter>
